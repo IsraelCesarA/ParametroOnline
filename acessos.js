e3e59ff6aeb75c325872cad1860da2f7c6fc8b39
Israel César <113929138+IsraelCesarA@users.noreply.github.com>
2025-09-08 22:03:17 -0300

    Update acessos.js

--- acessos.js
+++ acessos.js
@@ -1,7 +1,18 @@
 export const acessos = {
-    "auxiliar": "minhasenhasecretadeauxiliar",
-    "supervisor": "minhasenhamaissecretadesupervisor",
-    "teste": "123",
+    "auxiliar": "naocompartilhe",
+    "supervisor": "E61051",
+    "teste": "996465220",
     "israel": "1532",
+    "fortaleza": "E02$#g",
+    "saojose": "E12$#h",
+    "siaragrande": "E14$#k",
+    "santamaria": "E20$#l",
+    "aliança": "E21$#m",
+    "maraponga": "E26$#p",
+    "viaurbana": "E30$#q",
+    "vega": "E35$#r",
+    "dragão": "E42$#t",
+    "joao": "787",
+    "cco": "E61$#w",
 };
 
